73996042a76cc31cb64316e05b0eb2ae937b1e29
sujeebadal <sujeebadal@gmail.com>
2019-01-28 16:33:18 -0500

    new comment added

---
+++ src/HelloWorld.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println ("Hello world!");
+        // Declare an integer variable called num and
+        //assigned it a start value of 0
+        int num = 0;
+
+        Scanner Keyboard = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        num = Keyboard.nextInt();
+        System.out.println(num);
+
+        //new comment
+    }
+}
+
+
+/* Muli-line comment
+.
+.
+ */
